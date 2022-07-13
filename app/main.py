9c3abcf82e92b1b02ce8df7d6d20d2347fc3e8ce
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-13 21:57:23 +0100

    setting up parser and testing captions

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 import os
 import openai
+import argparse
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+def main():
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("--input", "-i", type=str, required=True)
+    args = argParser.parse_args()
+    input = args.input
+    print(f"Input : {input}")
+    generate_instagram_caption(input)
 
-response = openai.Completion.create(engine="davinci", prompt="testing", max_tokens=5)
 
-print(response)
+def generate_instagram_caption(theme: str):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    prompt = f"Generate an instagram caption related to {theme} : "
+    response = openai.Completion.create(
+        engine="davinci-instruct-beta-v3", prompt=prompt, max_tokens=50)
+
+    print(response["choices"][0]["text"].strip())
+
+
+if __name__ == "__main__":
+    main()
