7a515a787a6fa9d67cde5dbef38fae2f52c88d5d
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-13 23:06:46 +0100

    implemented the generated caption and generated words

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import os
+from typing import List
 import openai
 import argparse
+import re
 
 
 def main():
@@ -9,16 +11,43 @@ def main():
     args = argParser.parse_args()
     input = args.input
     print(f"Input : {input}")
-    generate_instagram_caption(input)
+    caption_result = generate_instagram_caption(input)
+    related_words_result = generate_related_words(input)
+    print(caption_result)
+    print(related_words_result)
 
 
-def generate_instagram_caption(theme: str):
+def generate_related_words(theme: str) -> List[str]:
+    # Load the API key generated from OpenAPI
     openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    prompt = f"Generate related caption words to {theme} : "
+    response = openai.Completion.create(
+        model="text-davinci-002", prompt=prompt, max_tokens=100)
+    words = response["choices"][0]["text"]
+    words = words.strip()
+
+    words_list = re.split("-|,|\n", words)
+
+    # Clean generated list of words
+    words_list = [w.upper() for w in words_list if len(w)]
+
+    return words_list
+
+
+def generate_instagram_caption(theme: str) -> str:
+    # Load the API key generated from OpenAPI
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
     prompt = f"Generate an instagram caption related to {theme} : "
     response = openai.Completion.create(
         engine="davinci-instruct-beta-v3", prompt=prompt, max_tokens=50)
+    caption = response["choices"][0]["text"]
 
-    print(response["choices"][0]["text"].strip())
+    # Clean generated caption
+    caption = caption.strip()
+
+    return caption
 
 
 if __name__ == "__main__":
