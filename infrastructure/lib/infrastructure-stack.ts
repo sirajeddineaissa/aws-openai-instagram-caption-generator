8016cb7a5187e34bcd78f9fbb1af30c8438a8d79
sirajeddineaissa <sirajeddineaissa@protonmail.com>
2022-07-16 18:02:05 +0100

    added aws-lambda-python-alpha module for packaging problems

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,19 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambda from '@aws-cdk/aws-lambda-python-alpha';
 import * as dotenv from "dotenv"
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+
+dotenv.config();
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const layer = new lambda.LayerVersion(this, "LambdaLayer", {
-      code: lambda.Code.fromAsset("lambda/lambda_layer.zip"),
-      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+    const layer = new lambda.PythonLayerVersion(this, "LambdaLayer", {
+      entry: "/lambda/lambda_layer.zip",
+      compatibleRuntimes: [Runtime.PYTHON_3_9],
     });
 
-    const lambdaAPI = new lambda.Function(this, "ApiFunction", {
-      runtime: lambda.Runtime.PYTHON_3_9,
-      code: lambda.Code.fromAsset("../app/"), // Code directory
+    const lambdaAPI = new lambda.PythonFunction(this, "ApiFunction", {
+      runtime: Runtime.PYTHON_3_9,
+      entry: "../app/", // Code directory
       handler: "api.handler", // Handler to wrap the API 
       layers: [layer],
       environment: {
